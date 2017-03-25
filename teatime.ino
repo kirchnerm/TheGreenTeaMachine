012474c78196b33e285860f9a5844408616b0ef9
m.kirchner <kirchner.m@gmail.com>
2017-03-25 21:09:26 +0100

    threshold and target temp waiting implemented

--- teatime.ino
+++ teatime.ino
@@ -1,15 +1,19 @@
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
-#define ONE_WIRE_BUS 6 
-#define BUTTON 2 // interrupt 0
+#define ONE_WIRE_BUS 6
+#define BUTTON 2
 #define BUZZER 5
+
+#define WAITING_FOR_THRESHOLD 0
+#define WAITING_FOR_TARGET_TEMP 1
 
 int LEDPins[] = { 8, 9, 10 };
 int temps[] = { 70, 80, 90 };
 int availableTemps = 3;
 
 int targetTemp = 1; // 80 Degree default
+int state = WAITING_FOR_THRESHOLD;
 
 unsigned int previousTime = 0, prellTime = 500;
 
@@ -18,10 +22,10 @@ DallasTemperature sensors(&oneWire);
 
 void setup() {
   pinMode(BUTTON, INPUT);
-  digitalWrite(BUTTON, LOW); 
+  digitalWrite(BUTTON, LOW);
   attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressed, HIGH);
 
-  for(int i=0; i < availableTemps; i++) {
+  for (int i = 0; i < availableTemps; i++) {
     pinMode(LEDPins[i], OUTPUT);
   }
 
@@ -31,44 +35,54 @@ void setup() {
   Serial.println("TeaTime!");
 
   pinMode(BUZZER, OUTPUT);
-  tone(BUZZER, 1000); // Send 1KHz sound signal...
-  delay(1000);        // ...for 1 sec
-  noTone(BUZZER);     // Stop sound...
-  delay(1000);        // ...for 1sec
-
-   // Start up the library 
- sensors.begin(); 
+  playSound();
+  sensors.begin();
 }
 
 void loop() {
- Serial.print(" Requesting targetTemperatures..."); 
- sensors.requestTemperatures(); // Send the command to get targetTemperature readings 
- Serial.println("DONE"); 
- Serial.print("Temperature is: "); 
- Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
- delay(1000); 
+  sensors.requestTemperatures();
+  float actualTemp = sensors.getTempCByIndex(0);
+  Serial.print("Temperature: ");
+  Serial.println(actualTemp);
+
+  if (state == WAITING_FOR_THRESHOLD && actualTemp >= temps[targetTemp]) {
+    state = WAITING_FOR_TARGET_TEMP;
+  }
+
+  if (state == WAITING_FOR_TARGET_TEMP && actualTemp <= temps[targetTemp]) {
+    state = WAITING_FOR_THRESHOLD;
+    // buzzer !!!!
+    playSound();
+  }
+  delay(1000);
 }
 
 void buttonPressed() {
-  Serial.println(previousTime);  
+  Serial.println(previousTime);
   int buttonState = digitalRead(BUTTON);
-  if( buttonState == 1 && (millis() - previousTime) > prellTime ) {
+  if ( buttonState == 1 && (millis() - previousTime) > prellTime ) {
     previousTime = millis();
     targetTemp++;
-    if(targetTemp > availableTemps - 1) {
+    if (targetTemp > availableTemps - 1) {
       targetTemp = 0;
     }
     setTempLeds();
   }
 }
 
-void setTempLeds() {   
-     for(int i=0; i<availableTemps; i++) {
-      if(i==targetTemp) {
-        digitalWrite(LEDPins[i],HIGH);
-      } else {
-        digitalWrite(LEDPins[i],LOW);
-      }
+void setTempLeds() {
+  for (int i = 0; i < availableTemps; i++) {
+    if (i == targetTemp) {
+      digitalWrite(LEDPins[i], HIGH);
+    } else {
+      digitalWrite(LEDPins[i], LOW);
     }
+  }
 }
 
+void playSound() {
+  tone(BUZZER, 1000);
+  delay(1000);
+  noTone(BUZZER);
+  delay(1000);
+}
