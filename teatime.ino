c0006a952ff1cef8120f90c9c4d6e075cdd71f55
m.kirchner <kirchner.m@gmail.com>
2017-03-25 20:28:41 +0100

    added temperature lib and demo code

--- teatime.ino
+++ teatime.ino
@@ -1,3 +1,17 @@
+#include <DallasTemperature.h>
+
+#include <OneWire.h>
+
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 6 
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS);
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
 
 #define BUTTON_PIN 2 // interrupt 0
 #define BUZZER 5
@@ -29,11 +43,24 @@ void setup() {
   delay(1000);        // ...for 1 sec
   noTone(BUZZER);     // Stop sound...
   delay(1000);        // ...for 1sec
+
+   // Start up the library 
+ sensors.begin(); 
 }
 
 void loop() {
-  // temperature scanning here ...
-  //buttonPressed();
+ // call sensors.requestTemperatures() to issue a global temperature 
+ // request to all devices on the bus 
+/********************************************************************/
+ Serial.print(" Requesting temperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ Serial.println("DONE"); 
+/********************************************************************/
+ Serial.print("Temperature is: "); 
+ Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+   // You can have more than one DS18B20 on the same bus.  
+   // 0 refers to the first IC on the wire 
+   delay(1000); 
 }
 
 void buttonPressed() {
