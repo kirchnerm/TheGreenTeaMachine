39e7748053b940a79df93ef3736634ee8b407b2e
m.kirchner <kirchner.m@gmail.com>
2017-03-25 20:56:18 +0100

    refactored stuff

--- teatime.ino
+++ teatime.ino
@@ -1,36 +1,28 @@
 #include <DallasTemperature.h>
-
 #include <OneWire.h>
 
-// Data wire is plugged into pin 2 on the Arduino 
 #define ONE_WIRE_BUS 6 
-/********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices  
-// (not just Maxim/Dallas temperature ICs) 
-OneWire oneWire(ONE_WIRE_BUS);
-/********************************************************************/
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-
-
-#define BUTTON_PIN 2 // interrupt 0
+#define BUTTON 2 // interrupt 0
 #define BUZZER 5
 
-int ledPins[] = { 8, 9, 10 };
+int LEDPins[] = { 8, 9, 10 };
 int temps[] = { 70, 80, 90 };
 int availableTemps = 3;
 
-int temp = 1; // 80 Degree default
+int targetTemp = 1; // 80 Degree default
 
 unsigned int previousTime = 0, prellTime = 500;
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 void setup() {
-  pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, LOW); // internal PullUp to 5V
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressed, HIGH);
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, LOW); 
+  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressed, HIGH);
 
   for(int i=0; i < availableTemps; i++) {
-    pinMode(ledPins[i], OUTPUT);
+    pinMode(LEDPins[i], OUTPUT);
   }
 
   setTempLeds();
@@ -49,28 +41,22 @@ void setup() {
 }
 
 void loop() {
- // call sensors.requestTemperatures() to issue a global temperature 
- // request to all devices on the bus 
-/********************************************************************/
- Serial.print(" Requesting temperatures..."); 
- sensors.requestTemperatures(); // Send the command to get temperature readings 
+ Serial.print(" Requesting targetTemperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get targetTemperature readings 
  Serial.println("DONE"); 
-/********************************************************************/
  Serial.print("Temperature is: "); 
  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
-   // You can have more than one DS18B20 on the same bus.  
-   // 0 refers to the first IC on the wire 
-   delay(1000); 
+ delay(1000); 
 }
 
 void buttonPressed() {
   Serial.println(previousTime);  
-  int buttonState = digitalRead(BUTTON_PIN);
+  int buttonState = digitalRead(BUTTON);
   if( buttonState == 1 && (millis() - previousTime) > prellTime ) {
     previousTime = millis();
-    temp++;
-    if(temp > availableTemps - 1) {
-      temp = 0;
+    targetTemp++;
+    if(targetTemp > availableTemps - 1) {
+      targetTemp = 0;
     }
     setTempLeds();
   }
@@ -78,10 +64,10 @@ void buttonPressed() {
 
 void setTempLeds() {   
      for(int i=0; i<availableTemps; i++) {
-      if(i==temp) {
-        digitalWrite(ledPins[i],HIGH);
+      if(i==targetTemp) {
+        digitalWrite(LEDPins[i],HIGH);
       } else {
-        digitalWrite(ledPins[i],LOW);
+        digitalWrite(LEDPins[i],LOW);
       }
     }
 }
