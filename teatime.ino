393fb76f523d96168eb0c8a8def6b28933d290e3
m.kirchner <kirchner.m@gmail.com>
2017-03-25 18:05:34 +0100

    working 3 LED switchable by the button

--- teatime.ino
+++ teatime.ino
@@ -6,37 +6,48 @@ int availableTemps = 3;
 
 int temp = 1; // 80 Degree default
 
-unsigned int previousTime = 0, prellTime = 20;
+unsigned int previousTime = 0, prellTime = 500;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, HIGH); // internal PullUp to 5V
-  attachInterrupt(0, buttonPressed, LOW);
+  digitalWrite(BUTTON_PIN, LOW); // internal PullUp to 5V
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressed, HIGH);
 
   for(int i=0; i < availableTemps; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
 
+  setTempLeds();
+
+  Serial.begin(9600);
+  Serial.println("TeaTime!");
 }
 
 void loop() {
   // temperature scanning here ...
-
+  //buttonPressed();
 }
 
 void buttonPressed() {
-  if( (millis() - previousTime) > prellTime ) {
+  Serial.println(previousTime);  
+  int buttonState = digitalRead(BUTTON_PIN);
+  if( buttonState == 1 && (millis() - previousTime) > prellTime ) {
+    previousTime = millis();
     temp++;
     if(temp > availableTemps - 1) {
       temp = 0;
     }
-    for(int i=0; i<availableTemps; i++) {
+    setTempLeds();
+  }
+}
+
+void setTempLeds() {   
+     for(int i=0; i<availableTemps; i++) {
       if(i==temp) {
         digitalWrite(ledPins[i],HIGH);
       } else {
         digitalWrite(ledPins[i],LOW);
       }
     }
-  }
 }
 
